9712bbc07e01f521e4463d9d0d655d45a13cf633
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2024-02-21 05:06:40 +0000

    add prettier config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Bird",
+      name: "Star",
+      breed: "Crow",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Alloys",
+      breed: "GoodCat",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
