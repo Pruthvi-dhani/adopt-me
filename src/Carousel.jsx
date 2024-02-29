9bbc5cff63f2de1bf415ee6322a34ed822662c70
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2024-02-29 22:23:48 +0000

    added class component

---
+++ src/Carousel.jsx
@@ -0,0 +1,35 @@
+// example of class component - whenever possible avoid class components and use functional components
+// since thats more popular these days
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal hero" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
